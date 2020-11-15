06cb357985a9f5cff2aadba0671c404e5f44a073
Marcin Krysiński <marcin.krysik11@gmail.com>
2020-11-15 19:40:38 +0100

    CRUD operations for authors and books

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-// import { PrismaClient } from '@prisma/client'
+import booksRouter from './routes/books';
+import authorsRouter from './routes/authors';
 import { config } from 'dotenv';
+
 config();
 
 const app = express();
-// const prisma = new PrismaClient();
+
 app.use(bodyParser.json());
 
 const PORT = process?.env?.PORT || 4000;
+app.use('/api/books', booksRouter);
+app.use('/api/authors', authorsRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
---
+++ src/routes/books.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import prisma from '../prisma';
+
+const router = express.Router();
+
+router.get('', async (_req, res) => {
+  try {
+    const books = await prisma.books.findMany({ include: { author: true } });
+    res.status(200).json(books);
+  } catch (err) {
+    res.json({ok: false, message: err.message})
+  } finally {
+    await prisma.$disconnect();
+  }
+});
+
+router.post('', async (req, res) => {
+  const book = req.body.book;
+  try {
+    if (book) {
+      const result = await prisma.books.create({
+        data: book,
+      })
+      res.status(201).json(result);
+    } else {
+      res.status(400).send('Bad request parameters');
+    }
+  } catch (err) {
+    res.json({ ok: false, message: err.message })
+  } finally {
+    await prisma.$disconnect();
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const book = req.body.book;
+    const bookId = +req.params.id;
+    const result = await prisma.books.update({
+      where: { id: bookId },
+      data: book
+    });
+    res.status(200).json({ ok: true, result })
+  } catch (err) {
+    res.json({ok: false, message: err.message})
+  } finally {
+    await prisma.$disconnect();
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const bookId = +req.params.id;
+    const result = await prisma.books.delete({
+      where: { id: bookId }
+    });
+    res.status(200).json({ ok: true, result })
+  } catch (err) {
+    res.json({ ok: false, message: err.message })
+  } finally {
+    await prisma.$disconnect();
+  }
+})
+
+export default router;
